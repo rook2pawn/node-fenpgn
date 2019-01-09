7c150419e4d13e635c6c051d5294993d14c5e916
rook2pawn <rook2pawn@gmail.com>
2019-01-09 08:39:39 -0800

    update

--- test/testGetAllMoves.js
+++ test/testGetAllMoves.js
@@ -7,25 +7,6 @@ t('test get All moves', function(test) {
     var game = new fen;
     var moves = game.allMoves(); // based on active player.
     test.deepEquals(moves,
-        [ 'b1a3',
-  'b1c3',
-  'g1f3',
-  'g1h3',
-  'a2a3',
-  'a2a4',
-  'b2b3',
-  'b2b4',
-  'c2c3',
-  'c2c4',
-  'd2d3',
-  'd2d4',
-  'e2e3',
-  'e2e4',
-  'f2f3',
-  'f2f4',
-  'g2g3',
-  'g2g4',
-  'h2h3',
-  'h2h4' ]);
+        [ 'a2a3', 'a2a4', 'b2b3', 'b2b4', 'c2c3', 'c2c4', 'd2d3', 'd2d4', 'e2e3', 'e2e4', 'f2f3', 'f2f4', 'g2g3', 'g2g4', 'h2h3', 'h2h4', 'b1a3', 'b1c3', 'g1f3', 'g1h3' ]);
     console.log("evaluate board:", game.evaluateBoard(game.stateLive().board));
 });
--- test/testIsCheckMove.js
+++ test/testIsCheckMove.js
@@ -2,7 +2,8 @@ var fen = require('../index');
 var h = require('../lib/help');
 var t = require('tape');
 t('testIsCheckMove',function(test) {
-    test.plan(7);
+    // check where a checkmate can be removed
+    test.plan(9);
     var game = new fen();
     game.mm('e2e4');
     test.equals(game.last().blackChecked,false);
@@ -16,7 +17,9 @@ t('testIsCheckMove',function(test) {
     game.mm('e6d7');
     test.equals(game.getActivePlayer(),'black')
     test.equals(game.last().blackChecked,true);
-    test.equals(game.last().winner,"w");
+    test.equals(game.last().winner,"", "there should be no winner yet");
+    test.equals(game.last().winner,"", "there should be no winner yet");
+    test.deepEquals(h.allMoves(game.last()), [ 'b8d7', 'c8d7', 'd8d7', 'e8d7' ], "these are the outs for black");
     // check that history hasn't been screwed with
     var hist = game.getHistory();
     // 7 moves + 1 startpos history
