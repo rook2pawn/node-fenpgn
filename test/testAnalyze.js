0e3fb5b3c5a636ebc9207dd85cfdb8885a1d8309
rook2pawn <rook2pawn@gmail.com>
2019-01-11 03:07:45 -0800

    update

--- test/testAnalyze.js
+++ test/testAnalyze.js
@@ -1,17 +1,11 @@
 var fen = require('../index');
 var t = require('tape');
 
-t('test analyze', function(test) {
-    test.plan(1);
+t('test analyze2', function(test) {
+    test.plan(2);
     var analyze = fen.analyze;
-    let result = analyze("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR");
-    test.pass();
-});
-
-t.only('test analyze2', function(test) {
-    test.plan(1);
-    var analyze = fen.analyze;
-    let result = analyze("8/8/8/8/r7/n1b5/1B6/8");
-    console.log("result:", result)
-    test.pass();
+    const result = analyze("8/8/8/8/8/r7/n1b5/1B6", "black");
+    test.equals(result, "c2b1")
+    const result2 = analyze("8/8/8/8/8/r7/n1b5/1B6", "white");
+    test.equals(result2, "b1c2")
 });
