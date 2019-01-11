bc9626050a9d673f86f2b0b86134dd57895a142e
rook2pawn <rook2pawn@gmail.com>
2019-01-10 19:27:55 -0800

    update

--- test/testAnalyze.js
+++ test/testAnalyze.js
@@ -5,6 +5,13 @@ t('test analyze', function(test) {
     test.plan(1);
     var analyze = fen.analyze;
     let result = analyze("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR");
+    test.pass();
+});
+
+t.only('test analyze2', function(test) {
+    test.plan(1);
+    var analyze = fen.analyze;
+    let result = analyze("8/8/8/8/r7/n1b5/1B6/8");
     console.log("result:", result)
     test.pass();
 });
